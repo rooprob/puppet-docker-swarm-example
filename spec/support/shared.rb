ff93a9f3b682e5eb25ce45e97c1ef423d8583406
Robert Fielding <fieldingrp@gmail.com>
2015-11-20 21:18:46 +1300

    Fix test for correctness and style

--- spec/support/shared.rb
+++ spec/support/shared.rb
@@ -6,7 +6,7 @@ C2 = "#{NETWORK}-c2"
 
 RSpec.shared_examples 'docker' do
   describe command('docker version') do
-    its(:stdout) { should match /Version:\s+1\.9/ }
+    its(:stdout) { should match(/Version:\s+1\.9/) }
   end
   describe port('2375') do
     it { should be_listening.with('tcp6') }
@@ -21,7 +21,7 @@ end
 
 RSpec.shared_examples 'consul' do
   describe command('consul version') do
-    its(:stdout) { should match /Consul v0\.5\.\d+/ }
+    its(:stdout) { should match(/Consul v0\.5\.\d+/) }
   end
   describe port('8400') do
     it { should be_listening.with('tcp6') }
@@ -35,7 +35,7 @@ RSpec.shared_examples 'consul' do
   end
 end
 
-RSpec.shared_examples 'containers' do |c1,c2,network|
+RSpec.shared_examples 'containers' do |c1, c2, network|
   context "container #{c1} has hostname" do
     describe command("docker -H :3000 exec -i #{c1} hostname") do
       its(:stdout) { should match c1 }
@@ -48,18 +48,18 @@ RSpec.shared_examples 'containers' do |c1,c2,network|
   end
   context "container #{c1} pings self" do
     describe command("docker -H #{PRIMARY}:3000 exec -i #{c1} ping -c 1 #{c1}") do
-      its(:stdout) { should match /1 packets transmitted, 1 packets received, 0% packet loss/ }
+      its(:stdout) { should match(/1 packets transmitted, 1 packets received, 0% packet loss/) }
     end
     xdescribe command("docker -H #{PRIMARY}:3000 exec -i #{c1} ping -c 1 #{c1}.#{network}") do
-      its(:stdout) { should match /1 packets transmitted, 1 packets received, 0% packet loss/ }
+      its(:stdout) { should match(/1 packets transmitted, 1 packets received, 0% packet loss/) }
     end
   end
   context "container #{c1} pings peer #{c2}" do
     describe command("docker -H #{PRIMARY}:3000 exec -i #{c1} ping -c 1 #{c2}") do
-      its(:stdout) { should match /1 packets transmitted, 1 packets received, 0% packet loss/ }
+      its(:stdout) { should match(/1 packets transmitted, 1 packets received, 0% packet loss/) }
     end
     describe command("docker -H #{PRIMARY}:3000 exec -i #{c1} ping -c 1 #{c2}.#{network}") do
-      its(:stdout) { should match /1 packets transmitted, 1 packets received, 0% packet loss/ }
+      its(:stdout) { should match(/1 packets transmitted, 1 packets received, 0% packet loss/) }
     end
   end
 end
@@ -67,12 +67,12 @@ end
 RSpec.shared_examples 'cluster' do
   # docker via swarm on primary
   describe command("docker -H #{PRIMARY}:3000 info") do
-    its(:stdout) { should match /Nodes: 2/ }
+    its(:stdout) { should match(/Nodes: \d+/) }
   end
   describe command('consul members') do
     its(:stdout) do
-      should match /swarm-1\s+\S+:8301\s+alive/
-      should match /swarm-2\s+\S+:8301\s+alive/
+      should match(/swarm-1\s+\S+:8301\s+alive/)
+      should match(/swarm-2\s+\S+:8301\s+alive/)
     end
   end
 
@@ -84,7 +84,7 @@ RSpec.shared_examples 'cluster' do
 
   # XXX returns one IP even if swarm & manger constainers are both running
   describe command('docker inspect --format \'{{ .NetworkSettings.IPAddress }}\' swarm') do
-    its(:stdout) { should match /^\d+\.\d+\.\d+\.\d+$/ }
+    its(:stdout) { should match(/^\d+\.\d+\.\d+\.\d+$/) }
   end
 end
 
